2f79149c98ad2b6ee27c76fa860ccf84d2613fb2
Geoffrey <geo.bisch@gmail.com>
2020-09-29 14:52:06 +0200

    bourrin!!!!

---
+++ App1/UnitTestApp1/obj/Debug/90/android/src/crc64a6ca00e260fa1167/MainActivity.java
@@ -0,0 +1,47 @@
+package crc64a6ca00e260fa1167;
+
+
+public class MainActivity
+	extends crc6488b3b0f9b9bb2e01.RunnerActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("UnitTestApp1.MainActivity, UnitTestApp1", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("UnitTestApp1.MainActivity, UnitTestApp1", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
